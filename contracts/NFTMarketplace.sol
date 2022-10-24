ee2279ddf5b12082f709d1fc0d55432171c7465d
kelvincheung000 <cheungkamkit@gmail.com>
2022-10-25 00:36:14 +0800

    opened new nft project

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// INTERNAL IMPORT FOR NFT OPENZIPLINE
+import "@openzeppelin/contracts/utils/Counters.sol"; // using as a counter
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+constract NFTMarketplace is ERC721URIStorage{
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        unit256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() ERC721("NFT Metavarse Token", "MYNFT"){
+        
+    }
+}
