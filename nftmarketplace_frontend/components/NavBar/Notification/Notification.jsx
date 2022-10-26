ed931ed0c3585dce77ec19a0754ae8a7d6fa171d
kelvincheung000 <cheungkamkit@gmail.com>
2022-10-27 00:53:29 +0800

    updated developed codes

--- nftmarketplace_frontend/components/NavBar/Notification/Notification.jsx
+++ nftmarketplace_frontend/components/NavBar/Notification/Notification.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Image from "next/image";
+
+// INTERNAL IMPORT
+import Style from "./Notification.module.css";
+import images from "../../../img";
 
 const Notification = () => {
   return <div>Notification</div>;
